91b814910a09796a141ba01456999c6b50723715
Nathan Tarr <nmtarr@ncsu.edu>
2017-03-16 09:40:42 -0400

    I added two functions to the misc module.  RATdescription() creates a dictionary of measures of variability for a raster attribute table.  RATplot() creates and saves two plots that of the data in a raster attribute table.

--- gapanalysis/misc.py
+++ gapanalysis/misc.py
@@ -22,4 +22,132 @@ def MakeRemapList(mapUnitCodes, reclassValue):
     return remap 
     
     
+def RATplot(raster, OgiveName, DistributionName, dropMax=False, dropZero=False):
+    '''
+    (string, string, string, boolean, boolean) -> saved figures
+    
+    Creates and saves two figures that summarize a Raster Attribute Table (RAT).  The 
+        Ogive plot is a graph of value vs. cumulative frequency (count).  The 
+        distribution plot is a graph of the value vs. frequency (count).
+    
+    Argument:
+    raster -- A path to a raster with an attribute table (RAT) to summarize.
+    OgiveName -- Path and filename to use for the Ogive plot name. Give a ".png" suffix.
+    DistribtuionName -- Path and filename to use for the plot of value vs. count.
+        Give this a ".png" suffix.
+    dropMax -- True or False to drop the highest value from the table.  This is useful
+        when using richness rasters that included "counter pixels" in the NW corner.
+    dropZero -- True or False, will the row for zero values from the table before 
+        plotting.  
+    
+    Example:
+    >>> RATplot(raster="T:/temp/a_richness_map.tif", OgiveName="T:/temp/Ogive.png",
+                DistributionName="T:/temp/RATdist.png", dropMax=True, dropZero=True)
+    '''
+    import arcpy, pandas as pd
+    
+    # Create empty dataframe
+    DF0 = pd.DataFrame(index=[], columns=[])
+    DF0.index.name = "value"
+    
+    # Use search cursor to copy RAT to dataframe
+    rows = arcpy.SearchCursor(raster)
+    for row in rows:
+        frequency = row.getValue("COUNT")
+        value = row.getValue("VALUE")
+        DF0.loc[value, "freq"] = frequency
+        
+    # Drop max value
+    if dropMax == True:
+        # Drop highest value/counter
+        DF0 = DF0[:-1]
+    
+    # Drop zeros
+    if dropZero == True:
+        DF0 = DF0[DF0.index > 0]
+    
+    # Make Ogive plot
+    DF1 = DF0.copy()
+    DF1["cumFreq"] = DF1.freq.cumsum()
+    DF1.drop("freq", axis=1, inplace=True)
+    ax = DF1.plot(kind="line", legend=False)
+    ax.set_ylabel("cumulative frequency (cells)")
+    fig = ax.get_figure()
+    fig.savefig(OgiveName)
+    
+    # Make distribution plot
+    ax2 = DF0.plot(kind="line", legend=False)
+    ax2.set_ylabel("frequency (cells)")
+    fig2 = ax2.get_figure()
+    fig2.savefig(DistributionName)
+
+
+def RATdescription(raster, percentile_list, dropMax=False, dropZero=False):
+    '''
+    (string, list, boolean, boolean) -> dictionary
+    
+    Creates a dictionary of measures of variability for a Raster Attribute Table (RAT).
+        Includes mean, range (as a tuple), and percentile values from the list passed.
+    
+    Argument:
+    raster -- A path to a raster with an attribute table (RAT) to summarize.
+    percentile_list -- A python list of percentiles to calculate and include in the 
+        dictionary that is returned.
+    dropMax -- True or False to drop the highest value from the table.  This is useful
+        when using richness rasters that included "counter pixels" in the NW corner.
+    dropZero -- True or False, will the row for zero values from the table before 
+        plotting.  
+    
+    Example:
+    >>> RATdescription(raster="T:/temp/a_richness_map.tif", 
+                       percentile_list=[25, 50, 75],
+                       dropMax=True, 
+                       dropZero=True)
+    '''
+    import arcpy, pandas as pd
+    
+    # Create empty dataframe
+    DF0 = pd.DataFrame(index=[], columns=[])
+    
+    # Create dictionary for results
+    resultsDict = {}
+    
+    # Use search cursor to copy RAT to dataframe
+    rows = arcpy.SearchCursor(raster)
+    for row in rows:
+        frequency = row.getValue("COUNT")
+        value = row.getValue("VALUE")
+        DF0.loc[value, "freq"] = frequency
+    
+    if dropMax == True:
+        # Drop highest value/counter
+        DF0 = DF0[:-1]
+    
+    if dropZero == True:
+        DF0 = DF0[DF0.index > 0]
+    
+    # Calculate mean value
+    DF0.index.name = "value"
+    DF0.reset_index(drop=False, inplace=True)
+    DF0["countXvalue"] = DF0.value * DF0.freq
+    mean = DF0.countXvalue.sum()/DF0.freq.sum()
+    resultsDict["mean"] = mean
+    
+    # Calculate the range
+    _min = DF0.index.min()
+    _max = DF0.index.max()
+    _range = _min, _max
+    resultsDict["range"] = _range
+    
+    # Find percentile values
+    DF0.drop("countXvalue", inplace=True, axis=1)
+    DF0["cumFreq"] = DF0.freq.cumsum()
+    for percentile in percentiles:
+        percentile_freq = DF0.freq.sum()*(percentile/100.)
+        percentile_value = DF0.cumFreq.searchsorted(percentile_freq)[0]
+        resultsDict[str(percentile) + "th"] = percentile_value
+    
+    # Return result 
+    return resultsDict    
+    
     
