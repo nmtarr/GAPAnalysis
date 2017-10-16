63374f02a0aad2cb3f24e3b23b04747d2cb7d883
Nathan Tarr <nmtarr@ncsu.edu>
2017-10-16 13:40:45 -0400

    Edited setup.py License, and Readme per Python Packaging User Guide, which uses setuptools.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='GAPAnalysis',
     version='0.3',
-    packages=['gapanalysis'],
+    packages=find_packages(exclude=[docs, tests],
     author='Nathan M. Tarr',
     author_email='nmtarr@ncsu.edu',
     scripts=['bin/Update_Help_Files.py'],
@@ -11,8 +11,11 @@ setup(
     license='LICENSE.txt',
     description='Functions for analyzing Gap Analysis Program habitat maps.',
     long_description=open('README.txt').read(),
-    install_requires=[
-            "pandas >= 0.20.1",
-            "scipy >= 0.19.0",
-    ],
+    install_requires=["pandas >= 0.20.1","scipy >= 0.19.0"],
+    classifiers=['Development Status :: 3 - Alpha',
+    		'Programming Language :: Python :: 2.7'],
+    keywords='USGS Gap Analysis Program',
+    python_requires='==2.7',
+    # Could the below be used to point to ScienceBase?
+    #data_files=[('my_data', ['data/data_file'])],
 )
