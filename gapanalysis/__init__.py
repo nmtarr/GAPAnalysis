4d678d718db4ac040f88185b1ddaab19ca7a6d58
Nathan Tarr <nmtarr@ncsu.edu>
2023-05-08 12:57:10 -0400

    Commented out code until it can be vetted.

--- gapanalysis/__init__.py
+++ gapanalysis/__init__.py
@@ -1,3 +1,3 @@
-import landcover, misc, richness, data, habitat, docs
+from gapanalysis import landcover, misc, richness, data, habitat, docs
 
 __all__ = ['landcover', 'misc', 'richness', 'data', 'habitat', 'docs']
